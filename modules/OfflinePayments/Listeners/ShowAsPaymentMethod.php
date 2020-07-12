4ed563f18fb0d4cd148c42e7afb70750a4c3d206
“Emmanuel <emmarthurson@gmail.com>
2020-07-12 09:30:46 +0000

    hotfix

--- modules/OfflinePayments/Listeners/ShowAsPaymentMethod.php
+++ modules/OfflinePayments/Listeners/ShowAsPaymentMethod.php
@@ -2,6 +2,7 @@
 
 namespace Modules\OfflinePayments\Listeners;
 
+use App\Utilities\Modules;
 use App\Events\Module\PaymentMethodShowing as Event;
 
 class ShowAsPaymentMethod
@@ -16,8 +17,28 @@ class ShowAsPaymentMethod
     {
         $methods = json_decode(setting('offline-payments.methods'), true);
 
-        foreach ($methods as $method) {
-            $event->modules->payment_methods[] = $method;
+        try {
+            foreach ($methods as $method) {
+                $event->modules->payment_methods[] = $method;
+            }
+        } catch (\Throwable $th) {
+
+            $payment_method = [
+                'code' => 'offline-payments.cash.1',
+                'name' => 'Cash',
+                'customer' => 1,
+                'order' => 1,
+                'description' => 'Cash Payments',
+            ];
+
+            $methods[] = $payment_method;
+
+            setting()->set('offline-payments.methods', json_encode($methods));
+
+            setting()->save();
+
+            Modules::clearPaymentMethodsCache();
         }
+
     }
 }
