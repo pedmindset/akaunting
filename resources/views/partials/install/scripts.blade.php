04949f40c2a6035f4b2fb79e58391787c783aa12
“Emmanuel <emmarthurson@gmail.com>
2020-07-15 10:56:34 +0000

    hotfix

--- resources/views/partials/install/scripts.blade.php
+++ resources/views/partials/install/scripts.blade.php
@@ -5,7 +5,19 @@
 <script src="{{ asset('public/vendor/js-cookie/js.cookie.js') }}"></script>
 
 <script src="{{ asset('public/js/install.js?v=' . version('short')) }}"></script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5f0edb5d5b59f94722bac432/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 @stack('body_css')
 
 @stack('body_stylesheet')
